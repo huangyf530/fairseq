863678f322c6b189979991a3b83b300b8c4d5d5b
huangyf530 <lancerken530@outlook.com>
2021-07-24 06:17:35 +0000

    data preparation

--- scripts-mine/download-bpe.sh
+++ scripts-mine/download-bpe.sh
@@ -1,13 +1,15 @@
 # /bin/bash!
-# mkdir -p gpt2_bpe
-# wget -O gpt2_bpe/encoder.json https://dl.fbaipublicfiles.com/fairseq/gpt2_bpe/encoder.json
-# wget -O gpt2_bpe/vocab.bpe https://dl.fbaipublicfiles.com/fairseq/gpt2_bpe/vocab.bpe
+if [ ! -d gpt2_bpe ];then
+mkdir -p gpt2_bpe
+wget -O gpt2_bpe/encoder.json https://dl.fbaipublicfiles.com/fairseq/gpt2_bpe/encoder.json
+wget -O gpt2_bpe/vocab.bpe https://dl.fbaipublicfiles.com/fairseq/gpt2_bpe/vocab.bpe
+fi
 for SPLIT in train valid test; do \
     python -m examples.roberta.multiprocessing_bpe_encoder \
         --encoder-json gpt2_bpe/encoder.json \
         --vocab-bpe gpt2_bpe/vocab.bpe \
-        --inputs ../bert-corpus/bert-corpus.${SPLIT}.raw \
-        --outputs ../bert-corpus/bert-corpus.${SPLIT}.bpe \
+        --inputs /home/huangyufei/disk3/OpenWebText/openwebtext-raw/openwebtext.${SPLIT}.tokens \
+        --outputs /home/huangyufei/disk3/OpenWebText/openwebtext-raw/openwebtext.${SPLIT}.bpe \
         --keep-empty \
         --workers 60; \
 done
--- scripts-mine/preprocess-data.sh
+++ scripts-mine/preprocess-data.sh
@@ -2,8 +2,8 @@
 python fairseq_cli/preprocess.py \
     --only-source \
     --srcdict gpt2_bpe/dict.txt \
-    --trainpref ../bert-corpus/bert-corpus.train.bpe \
-    --validpref ../bert-corpus/bert-corpus.valid.bpe \
-    --testpref ../bert-corpus/bert-corpus.test.bpe \
-    --destdir data-bin/bert-corpus \
+    --trainpref /home/huangyufei/disk3/OpenWebText/openwebtext-raw/openwebtext.train.bpe \
+    --validpref /home/huangyufei/disk3/OpenWebText/openwebtext-raw/openwebtext.valid.bpe \
+    --testpref /home/huangyufei/disk3/OpenWebText/openwebtext-raw/openwebtext.test.bpe \
+    --destdir data-bin/openwebtext \
     --workers 40
