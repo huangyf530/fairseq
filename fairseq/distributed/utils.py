ae25454470f1febe9d41b16c3cdd9fbedeee9389
huangyf530 <lancerken530@outlook.com>
2021-08-19 21:14:17 +0800

    add checkpoint suffix for knowledge layers

--- fairseq/distributed/utils.py
+++ fairseq/distributed/utils.py
@@ -308,6 +308,8 @@ def distributed_init(cfg: FairseqConfig):
 
     if hasattr(cfg,  "model") and getattr(cfg.model, "base_layers", 0) > 0:
         cfg.checkpoint.checkpoint_suffix = f"-rank-{cfg.distributed_training.distributed_rank}"
+    if hasattr(cfg,  "model") and getattr(cfg.model, "knowledge_layers", 0) > 0:
+        cfg.checkpoint.checkpoint_suffix = f"-rank-{cfg.distributed_training.distributed_rank}"
 
     return cfg.distributed_training.distributed_rank
 
